26f76ab5780686a3be8af90596f5d46bc51b002d
revdhak <carlos256800@gmail.com>
2024-07-17 05:22:19 -0600

    Refactor EditButtons component and add delete functionality

--- src/components/EditButtons/EditButtons.tsx
+++ src/components/EditButtons/EditButtons.tsx
@@ -1,9 +1,14 @@
+import { useParams } from 'react-router'
+import { useGardenActions } from '../../hooks'
 import './EditButtons.css'
 
 export const EditButtons: React.FC = () => {
+  const { plantId } = useParams()
+  const { removeGrowLog } = useGardenActions()
   return (
     <div className='buttons-edit'>
       <button
+        onClick={() => removeGrowLog(plantId as string)}
         className='delete-btn'
       >DELETE
       </button>
